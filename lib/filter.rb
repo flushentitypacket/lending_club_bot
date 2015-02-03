e7880141784e73e845565c95450478c12b6dddef
Ronald Hong <ronniejhong@gmail.com>
2015-02-02 20:07:29 -0500

    Add interest rate filter, tighten dti requirements.

--- lib/filter.rb
+++ lib/filter.rb
@@ -2,8 +2,11 @@ class Filter
   def self.filter?(loan)
     loan.fico_range_low < 690 ||
     loan.delinq2_yrs > 0 ||
-    loan.dti > 20.0 ||
+    loan.dti > 15.0 ||
     loan.loan_amount > 20_000 ||
-    (loan.revol_bal / loan.loan_amount) > 0.5
+    (loan.revol_bal / loan.loan_amount) > 0.5 ||
+    loan.int_rate < 12.0
+    # loan.bought? # Idea for future filter to prevent repeated
+    #              # purchasing of the same note
   end
 end
