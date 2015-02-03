34f98c83c47399115094a95b8417079bc097dc82
Ronald Hong <ronniejhong@gmail.com>
2015-02-02 20:09:23 -0500

    Rid scoring system. Filter-only works better for now.

--- lib/lending_club_bot.rb
+++ lib/lending_club_bot.rb
@@ -1,8 +1,6 @@
 require 'lending_club'
 require 'yaml'
-require 'loan_with_score'
 require 'filter'
-require 'scorer'
 
 class LendingClubBot
 
@@ -10,22 +8,7 @@ class LendingClubBot
     load_credentials
     loans = LendingClub.loans
     filtered_loans = loans.reject { |loan| Filter.filter?(loan) }
-    loans_with_scores = filtered_loans.map do |loan|
-      LoanWithScore.new(loan, Scorer.score(loan))
-    end
-    top_loans = loans_with_scores.sort_by(&:score).reverse[0...5]
-    top_loans.each do |top_loan|
-      puts "score: #{top_loan.score}"
-      puts "int_rate: #{top_loan.loan.int_rate}"
-      puts "credit score: #{top_loan.loan.fico_range_low}"
-      puts "amount: #{top_loan.loan.loan_amount}"
-      puts
-    end
-    puts "number of loans that passed filter: #{filtered_loans.length}"
-    # loans_to_buy = loans_with_scores.select do |loan_with_score|
-    #   loan_with_score.score > threshold
-    # end
-    # purchases = order_loans(loans_to_buy)
+    # purchases = order_loans(filtered_loans)
     # notify(purchases)
   end
 
