cbb3ac1f525db77290b9aa113512ac93012876ed
Ronald Hong <ronniejhong@gmail.com>
2015-02-08 19:21:46 -0500

    Use them models!

--- lib/lending_club_bot.rb
+++ lib/lending_club_bot.rb
@@ -15,11 +15,18 @@ class LendingClubBot
   end
 
   def run
+    require_models
     loans = LendingClub.loans
     loans_to_buy = configuration.strategy.call(loans)
     # purchases = order_loans!(loans_to_buy)
     purchases = loans_to_buy
     configuration.notifier.notify(purchases) if configuration.notifier
+    nil
+  end
+
+  def self.require_models
+    Dir[File.dirname(__FILE__) + '/models/*.rb'].
+      each {|file| require file }
     nil
   end
 
