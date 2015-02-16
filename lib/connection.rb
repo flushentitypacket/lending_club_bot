f3764a51de966ef0aaed4b3c7707078f06525777
Ronald Hong <ronniejhong@gmail.com>
2015-02-16 15:26:04 -0500

    Simplify and tighten strictness of database setup.

--- lib/connection.rb
+++ lib/connection.rb
@@ -3,9 +3,7 @@ module Connection
     require "sequel"
     env = ENV['LENDING_CLUB_BOT_ENV'] || 'development'
     require 'yaml'
-    db_config = YAML.load_file('config/database.yml')
-    db_config = db_config[env] || db_config[env.to_sym] || db_config
-    db_config.keys.each{|k| db_config[k.to_sym] = db_config.delete(k)}
-    Sequel.connect(db_config)
+    config = YAML.load_file('config/database.yml')[env.to_sym]
+    Sequel.connect(config)
   end
 end
